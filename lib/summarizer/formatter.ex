ed760a135d587511b9584cbcb66e3f836ac96354
Aleksandar Dimov <isavitaisa@gmail.com>
2023-11-04 15:46:02 +0000

    Remove not needed comments

--- lib/summarizer/formatter.ex
+++ lib/summarizer/formatter.ex
@@ -5,19 +5,11 @@ defmodule Summarizer.Formatter do
 
   defp format_tree([head | tail], depth, parent) do
     is_directory = String.ends_with?(head, "/")
-
-    part_to_print =
-      if parent != "" do
-        String.replace_leading(head, parent, "")
-      else
-        head
-      end
-
+    part_to_print = if parent != "", do: String.replace_leading(head, parent, ""), else: head
     formatted_head = String.duplicate("  ", depth) <> part_to_print <> "\n"
     new_parent = if is_directory, do: head, else: parent
     count = if(is_directory, do: 1, else: 0)
     formatted_tail = format_tree(tail, depth + count, new_parent)
-    # Combine the formatted head and tail
     formatted_head <> formatted_tail
   end
 end
