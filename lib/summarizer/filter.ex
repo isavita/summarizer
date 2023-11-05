1aa95a0c1039ad2307ec18a7e61bba4f8d6a6bce
Aleksandar Dimov <isavitaisa@gmail.com>
2023-11-05 00:22:49 +0000

    Add files and directories to exclude for javascript/react

--- lib/summarizer/filter.ex
+++ lib/summarizer/filter.ex
@@ -1,7 +1,7 @@
 defmodule Summarizer.Filter do
   @allowed_extensions ~w(.py .js .jsx .ts .tsx .go .rb .java .cs .cpp .scss .sass .less .html .css .md .json .sh .xml .php .swift .kt .groovy .rs .ex .exs .elm .erl .hrl .lua .pl .pm .t .r .rmd .jl .c .h .m .mm .clj .cljs .cljc .hs .lhs .scala .sbt .d .dart .pas .prg .fp .aj .asm .bat .cmd .bash .fish .ps1 .vbs .rbw .vhd .vhdl .ucf .qsf .sdc .tcl .yml .yaml .ini .cfg .conf .toml .make .mk .dockerfile .v .sv .svh .uc .sls .slim .ejs .hbs .mustache .jade .pug .tpl .twig .liquid .haml .cfm)
   @specific_filenames ~w(Dockerfile Makefile)
-  @excluded_patterns ~r(deps/|/_build/|/node_modules/|/bin/|/test/|/dist/)
+  @excluded_patterns ~r(deps/|/_build/|/node_modules/|/bin/|/test/|/dist/|/fixtures/|/vendor/|/build/|/target/|/out/|/logs/|/tmp/|/cache/|/\.git/|/\.hg/|\/__tests__\/|/\.svn/|/\.DS_Store|/\.vscode|/\.idea/|/\.vagrant/|/\.sass-cache/|/\.gradle/|/\.settings/|/\.classpath/|/\.project/|/\.externalToolBuilders/|/\.c9/|/\.bundle/|/\.yardoc/|/\.yardopts/|/\.yardopts/)
 
   def filter_files(files) do
     Enum.filter(files, fn file ->
