4f501c8623a24b9f30c13b6dae712be474adf8b9
Aleksandar Dimov <isavitaisa@gmail.com>
2023-11-07 17:25:08 +0000

    Optimise the formatter

--- lib/summarizer/formatter.ex
+++ lib/summarizer/formatter.ex
@@ -1,29 +1,14 @@
 defmodule Summarizer.Formatter do
-  def format_tree(tree_list), do: format_tree(tree_list, 0, "")
+  def format_tree(tree_list), do: Enum.map_join(tree_list, "\n", &format_node/1)
 
-  defp format_tree([], _depth, _parent), do: ""
-
-  defp format_tree([head | tail], depth, parent) do
-    is_directory = String.ends_with?(head, "/")
-    part_to_print = if parent != "", do: String.replace_leading(head, parent, ""), else: head
-    formatted_head = String.duplicate("  ", depth) <> part_to_print <> "\n"
-    new_parent = if is_directory, do: head, else: parent
-    count = if(is_directory, do: 1, else: 0)
-    formatted_tail = format_tree(tail, depth + count, new_parent)
-    formatted_head <> formatted_tail
+  defp format_node(node) do
+    is_directory = String.ends_with?(node, "/")
+    depth = String.graphemes(node) |> Enum.filter(&(&1 == "/")) |> length()
+    indentation = String.duplicate("  ", depth)
+    indentation <> Path.basename(node) <> if(is_directory, do: "/", else: "")
   end
 
   def format_tree_xml(file_paths) do
-    """
-    <Files>
-    #{do_format_tree_xml(file_paths)}
-    </Files>
-    """
-  end
-
-  defp do_format_tree_xml([]), do: ""
-
-  defp do_format_tree_xml([head | tail]) do
-    "<File>#{head}</File>\n" <> do_format_tree_xml(tail)
+    "<Files>\n" <> Enum.map_join(file_paths, "\n", &"<File>#{&1}</File>") <> "\n</Files>"
   end
 end
